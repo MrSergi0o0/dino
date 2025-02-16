95ae76e3c150ec9ce8e078c9c766de840e97c035
MrSergi0o0 <sidorskijdmitro@gmail.com>
2025-02-16 13:24:45 +0200

    add class Player, class Sprite, player_image

--- game.py
+++ game.py
@@ -5,6 +5,8 @@ pygame.init()
 
 level1_objects = draw_level(level1)
 
+player = Player(50, H - 90, 40, 50, 11, player_images)
+
 game = True
 while game:
     for event in pygame.event.get():
@@ -13,6 +15,8 @@ while game:
 
     window.blit(bg, (0, 0))
     level1_objects.draw(window)
+    player.update()
+    player.draw()
     
     pygame.display.update()
     clock.tick(FPS)
